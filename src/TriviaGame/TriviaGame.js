040921179b671b29ff84d3851d0598f20c2b719b
therealDaze <danieldahlman@outlook.com>
2022-03-13 21:22:21 +0100

    Config: Setting up create react app

---
+++ src/TriviaGame/TriviaGame.js
@@ -0,0 +1,15 @@
+import styles from './TriviaGame.module.css';
+
+const TriviaGame = () => {
+  return (
+    <div className={styles.TriviaGame}>
+      <header className={styles.header}>
+        <p>
+          Hello trivia game
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default TriviaGame;
