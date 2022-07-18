b68295052a736c0fb5dcc39c63bd498dfab20dc6
ddahlman <danieldahlman@outlook.com>
2022-07-18 17:31:10 +0200

    LabeledSelect: WIP: styling the select

--- src/FormPage/LabeledSelect/LabeledSelect.js
+++ src/FormPage/LabeledSelect/LabeledSelect.js
@@ -4,24 +4,26 @@ const LabeledSelect = ({ name, label, value, onChange, options }) => {
   return (
     <label className={styles.label}>
       {label}
-      <select
-        className={styles.select}
-        name={name}
-        value={value}
-        onChange={onChange}
-      >
-        <option value="" disabled>
-          {label}
-        </option>
-        {options.map((option) => (
-          <option
-            key={option.id || option.label}
-            value={option.id || option.value}
-          >
-            {option.label}
+      <div className={styles.selectContainer}>
+        <select
+          className={styles.select}
+          name={name}
+          value={value}
+          onChange={onChange}
+        >
+          <option value="" disabled>
+            {label}
           </option>
-        ))}
-      </select>
+          {options.map((option) => (
+            <option
+              key={option.id || option.label}
+              value={option.id || option.value}
+            >
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </label>
   );
 };
